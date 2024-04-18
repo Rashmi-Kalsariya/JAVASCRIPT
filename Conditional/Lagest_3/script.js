7f15e1d51e5aca3577d98ec1e56176eee8731b44
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2024-04-18 23:47:49 +0530

    JS

---
+++ Conditional/Lagest_3/script.js
@@ -0,0 +1,18 @@
+
+let num1 = 5;
+let num2 = 9;
+let num3 = 7;
+
+
+
+if (num1 > num2 && num1 > num3) {
+    console.log("num1 is the Largest",num1);
+} 
+
+else if (num2 > num3) {
+    console.log("num2 is the Largest",num2);
+} 
+
+else {
+    console.log("num3 is the Largest",num3);
+}
