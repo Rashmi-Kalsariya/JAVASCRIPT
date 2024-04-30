72ee266e5e33972602ffb503a00b82292643100b
Kalsariya-Mrugen <mrugenk30@gmail.com>
2024-05-01 04:16:29 +0530

    JS

--- Loops/Even_sum/index.js
+++ Loops/Even_sum/index.js
@@ -1,17 +1,16 @@
 
 
-const Even_sum = (num) => {
 
+const showEvenSum=() =>{
+    const num = document.getElementById("num").value;
+    const sum = Even_sum(num);
+    document.getElementById("result").innerHTML = sum;
+}
+
+const Even_sum = (num) => {
     let sum = 0;
-    for (i = 2; i <= num; i += 2) {
+    for (let i = 2; i <= num; i += 2) {
         sum += i;
     }
     return sum;
 }
-
-
-let num = 6;
-
-let result = Even_sum(num);
-console.log(result);
-
