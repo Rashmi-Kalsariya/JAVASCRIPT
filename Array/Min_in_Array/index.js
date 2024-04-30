a9583a24fd7eb83c80a9833a81e9f113a290d7fc
rashmikalsariya02@gmail.com <mrugenk30@gmail.com>
2024-05-01 05:25:38 +0530

    JS

---
+++ Array/Min_in_Array/index.js
@@ -0,0 +1,18 @@
+
+
+const min = ()=>{
+    let arr=[8,2,3,5,6,4,7,-0,9]
+    let max=arr[0];
+    
+    for(let i=0;i<arr.length;i++){
+    
+        if (arr[i]<max) {
+            max=arr[i];
+        }
+    }
+    console.log(max)
+}
+
+min()
+
+
