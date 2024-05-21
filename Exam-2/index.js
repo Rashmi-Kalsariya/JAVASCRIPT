5f2efae7b450fe6dd1dbfac31cefc6dca1036371
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2024-05-21 07:03:07 +0530

    JS

--- Exam-2/index.js
+++ Exam-2/index.js
@@ -1,32 +1,104 @@
 /* FOR SALE */
 
-let countDownDate = new Date("May 20, 2024 10:15:00").getTime();
+const Count = ()=> {
+    let countDownDate = new Date().getTime() + (15 * 60 * 1000); 
 
-let time = setInterval(() => {
+    let time = setInterval(() => {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
 
-    let now = new Date("May 20, 2024 10:15:00").getTime();
+        if (distance > (15 * 60 * 1000)) {
+            countDownDate = now + (15 * 60 * 1000); 
+            distance = countDownDate - now; 
+        }
 
-    let distance = countDownDate - now;
+        let minutes = Math.floor(distance / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("Time").innerHTML = "Sale 50% Off" + "<br>" + minutes + " min " + seconds + " sec left" + "<br>" + "BUY NOW";
 
-    let SetTime = document.getElementById("Time");
+        if (distance <= 0) {
+            clearInterval(time);
+            document.getElementById("Time").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
 
-    if (SetTime) {
-        SetTime.innerHTML = days + "Day " + hours + "Hrs "
-            + minutes + "Min " + seconds + "Sec ";
-    }
-
-    if (distance < 0) {
-        clearInterval(time);
-        document.getElementById("Time").innerHTML = "EXPIRED";
-    }
-}, 1000);
+Count(); 
 
 
 /* Products */
 
 
+let products = []
+
+const uiMaker = () => {
+    document.getElementById("Products").innerHTML = ""
+    products.map((ele, i) => {
+
+        let title = document.createElement("h3")
+        title.innerHTML = ele.title
+
+        let img = document.createElement("img")
+        img.src = ele.img
+
+        let price = document.createElement("p")
+        price.innerHTML = ele.price
+
+        let div = document.createElement("div")
+        let Del = document.createElement("button");
+        Del.innerHTML = "Delete";
+        Del.style.cursor = "pointer";
+        Del.style.height = "30px";
+        Del.style.width = "60px"
+        Del.style.padding = "5px"
+        Del.style.margin = "5px"
+        Del.style.backgroundColor = "red"
+        Del.style.color = "white"
+        Del.style.fontWeight = "700"
+        Del.addEventListener("click", () => {
+            products.splice(i, 1);
+            uiMaker();
+        });
+
+        let Buy = document.createElement("button");
+        Buy.innerHTML = "Buy";
+        Buy.style.cursor = "pointer";
+        Buy.style.height = "30px";
+        Buy.style.width = "60px"
+        Buy.style.padding = "5px"
+        Buy.style.margin = "5px"
+        Buy.style.fontWeight = "700"
+        Buy.style.color = "white"
+        Buy.style.backgroundColor = "blue"
+        Buy.addEventListener("click", () => {
+            alert("You have bought: " + ele.title);
+        });
+
+        div.append(img, title, price, Del, Buy);
+        title.setAttribute("class", "title");
+        document.getElementById("Products").append(div);
+    });
+
+}
+
+const submit = (e) => {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let img = document.getElementById('img').value;
+    let price = document.getElementById('price').value;
+
+
+    let product = {
+        title: title,
+        img: img,
+        price: price
+    }
+
+    products.push(product);
+    uiMaker()
+}
+
+
+document.getElementById("DataAdd").addEventListener("submit", submit);
