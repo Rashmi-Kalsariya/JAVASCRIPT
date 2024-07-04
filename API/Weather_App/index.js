6cae35c31effdcdd82714625f5be1facc2994852
Rashmi-Kalsariya <rashmikalsariya02@gmail.com>
2024-07-04 09:17:55 +0530

    JS

--- API/Weather_App/index.js
+++ API/Weather_App/index.js
@@ -1,20 +1,49 @@
+let ImgData = {
+    Rain: "http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/256/Status-weather-showers-day-icon.png",
+    Clouds: "https://www.pngarts.com/files/3/White-Clouds-PNG-Pic.png",
+    Snow: "https://cdn-icons-png.freepik.com/512/6635/6635585.png",
+    Clear: "https://cdn-icons-png.flaticon.com/512/3222/3222807.png",
+    Thunderstorm: "https://icons.veryicon.com/png/o/weather/weather-fill/weather-thunderstorm-5.png",
+    Drizzle: "https://www.clipartmax.com/png/middle/182-1822480_rain-icon-rainy-weather-icon.png",
+    Haze: "https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-30-2682821_90800.png",
+    Sun: "https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3JtNTM1LXN1bi0wNy1hLnBuZw.png",
+    Others: "https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-30-2682821_90800.png"
+};
+
+let BgData = {
+    Rain: "url('https://images.pexels.com/photos/2448749/pexels-photo-2448749.jpeg?auto=compress&cs=tinysrgb&w=600')",
+    Clouds: "url('https://images.pexels.com/photos/531767/pexels-photo-531767.jpeg?auto=compress&cs=tinysrgb&w=600')",
+    Snow: "url('https://images.pexels.com/photos/4969844/pexels-photo-4969844.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load')",
+    Clear: "url('https://images.pexels.com/photos/5326741/pexels-photo-5326741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+    Thunderstorm: "url('https://images.pexels.com/photos/2531709/pexels-photo-2531709.jpeg?auto=compress&cs=tinysrgb&w=600')",
+    Drizzle: "url('https://images.pexels.com/photos/7485212/pexels-photo-7485212.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load')",
+    Haze: "url('https://images.pexels.com/photos/691574/pexels-photo-691574.jpeg?auto=compress&cs=tinysrgb&w=600')",
+    Sun: "url('https://images.pexels.com/photos/7093517/pexels-photo-7093517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')",
+    Others: "url('https://images.pexels.com/photos/1165991/pexels-photo-1165991.jpeg?auto=compress&cs=tinysrgb&w=600')"
+};
+
+
 const createWeatherCard = (data) => {
-    let Div = document.getElementById("data");
-    Div.innerHTML = '';
+    let div = document.getElementById("data");
+    div.innerHTML = '';
 
     let card = document.createElement("div");
     card.classList.add("weather-card");
 
-    let weatherImage = getWeatherImage(data.weather[0].main);
     let weatherDescription = data.weather[0].main;
+    
+    let weatherImageKey = ["Mist", "Dust", "Smoke", "Fog", "Sand", "Ash", "Squall", "Tornado"].includes(weatherDescription) ? "Others" : weatherDescription;
+    let weatherImage = ImgData[weatherImageKey] || ImgData.Others;
+    let backgroundImage = BgData[weatherImageKey] || BgData.Others;
+
+    document.body.style.backgroundImage = backgroundImage;
 
     card.innerHTML = `
         <h2>${data.name}, ${data.sys.country}</h2>
 
         <p>${new Date().toLocaleString()}</p>
         <p id="type">${weatherDescription}</p>
-
-        <img src="${weatherImage}" alt="Weather Icon" class="weather-icon">
+        <img src="${weatherImage}" alt="${weatherDescription}" class="weather-image">
 
         <p class="temperature">${Math.round(data.main.temp)}°</p>
         <p>Min: ${Math.round(data.main.temp_min)}° Max: ${Math.round(data.main.temp_max)}°</p>
@@ -39,29 +68,7 @@ const createWeatherCard = (data) => {
         </div>
     `;
 
-    Div.append(card);
-}
-
-const getWeatherImage = (weather) => {
-    if (weather.includes('Clear')) {
-        return 'https://cdn-icons-png.flaticon.com/512/3222/3222807.png';
-    } else if (weather.includes('Clouds')) {
-        return 'https://www.pngarts.com/files/3/White-Clouds-PNG-Pic.png';
-    } else if (weather.includes('Rain')) {
-        return 'http://icons.iconarchive.com/icons/oxygen-icons.org/oxygen/256/Status-weather-showers-day-icon.png';
-    } else if (weather.includes('Snow')) {
-        return 'https://cdn-icons-png.freepik.com/512/6635/6635585.png';
-    } else if (weather.includes('Thunderstorm')) {
-        return 'https://icons.veryicon.com/png/o/weather/weather-fill/weather-thunderstorm-5.png';
-    } else if (weather.includes('Drizzle')) {
-        return 'https://www.clipartmax.com/png/middle/182-1822480_rain-icon-rainy-weather-icon.png';
-    } else if (weather.includes('Mist') || weather.includes('Smoke') || weather.includes('Haze') || 
-               weather.includes('Dust') || weather.includes('Fog') || weather.includes('Sand') || 
-               weather.includes('Ash') || weather.includes('Squall') || weather.includes('Tornado')) {
-        return 'https://cdn.icon-icons.com/icons2/1370/PNG/512/if-weather-30-2682821_90800.png';
-    } else {
-        return 'https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3JtNTM1LXN1bi0wNy1hLnBuZw.png';
-    }
+    div.append(card);
 }
 
 const getData = async (city) => {
@@ -76,69 +83,28 @@ const HandleData = (e) => {
     getData(city);
 }
 
+const getWeather = async (lat, long) => {
+    let req = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=0fdc88f1a67d09059c9e313b7d58d6bc&units=metric`);
+    let res = await req.json();
+    createWeatherCard(res);
+}
+
+const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            console.log(position);
+            let lat = position.coords.latitude;
+            let long = position.coords.longitude;
+            console.log(lat, long);
+            getWeather(lat, long);
+        },
+        error => {
+            console.error(error);
+            alert("Unable to retrieve your location. Please enter the city name manually.");
+        }
+    );
+}
+
+getLocation();
+
 document.getElementById("form").addEventListener("submit", HandleData);
-
-
-/* SNOW FLAKES */
-
-class Flake {
-    constructor(m, x, y, c) {
-        this.mass = m;
-        this.position = createVector(x, y);
-        this.velocity = createVector(0, 0);
-        this.acceleration = createVector(0, 0);
-        this.c = color(c);
-    }
-    applyForce(force) {
-        const f = p5.Vector.div(force, this.mass);
-        this.acceleration.add(f);
-    }
-
-    update() {
-        this.velocity.add(this.acceleration);
-        this.velocity.limit(0.5);
-        this.position.add(this.velocity);
-        this.acceleration.mult(0);
-    }
-
-    display() {
-        noStroke();
-        fill(this.c);
-        ellipse(this.position.x, this.position.y, this.mass, this.mass);
-    }
-
-    checkEdges() {
-        if (this.position.x > (width + this.mass / 2)) {
-            this.position.x = (0 - this.mass / 2);
-        }
-        if (this.position.y > (height + this.mass / 2)) {
-            this.position.y = (0 - this.mass / 2);
-            this.position.x = (random(0, width) - this.mass / 2);
-        }
-    }
-}
-
-const flakes = [];
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    for (let i = 0; i < 70; i++) {
-        flakes.push(new Flake(random(3, 10), random(width), random(height), random(50, 200)));
-    }
-}
-
-function draw() {
-    background(10);
-
-    for (let i = 0; i < flakes.length; i++) {
-        const wind = (i % 2 === 0) ? createVector(0.1 * flakes[i].mass, 0) : createVector(-0.03 * flakes[i].mass, 0);
-        const gravity = createVector(0, 0.5 * flakes[i].mass);
-
-        flakes[i].applyForce(wind);
-        flakes[i].applyForce(gravity);
-
-        flakes[i].update();
-        flakes[i].display();
-        flakes[i].checkEdges();
-    }
-}
